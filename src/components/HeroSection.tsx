3902d18a0bf099c7765c9191fadac240ff0c3b46
vikas8669 <vikayadav8669@gmail.com>
2026-01-12 12:24:11 +0530

    first commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,21 @@
 
 
 const HeroSection = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="max-w-1200 overflow-hidden">
+            <div>
+                <video
+                className=""
+                    src='\newVideo.mp4' autoPlay
+                    muted
+                    loop
+                    playsInline />
+
+
+                <p className="text-green-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, reiciendis laboriosam?</p>
+            </div>
+        </div>
+    )
 }
 
 export default HeroSection
